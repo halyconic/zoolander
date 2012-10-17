e46365f72050fb654f55ccb12f81a15789707193
Nathan Murray <nkmurray@wisc.edu>
2012-10-16 23:16:50 -0500

    first attempt at readPage() done

--- buf.C
+++ buf.C
@@ -8,6 +8,7 @@
 #include "page.h"
 #include "buf.h"
 
+
 #define ASSERT(c)  { if (!(c)) { \
 		       cerr << "At line " << __LINE__ << ":" << endl << "  "; \
                        cerr << "This condition should hold: " #c << endl; \
@@ -25,7 +26,7 @@ BufMgr::BufMgr(const int bufs)
 
     bufTable = new BufDesc[bufs];
     memset(bufTable, 0, bufs * sizeof(BufDesc));
-    for (int i = 0; i < bufs; i++) 
+    for (int i = 0; i < bufs; i++)
     {
         bufTable[i].frameNo = i;
         bufTable[i].valid = false;
@@ -44,7 +45,7 @@ BufMgr::BufMgr(const int bufs)
 BufMgr::~BufMgr() {
 
     // flush out all unwritten pages
-    for (int i = 0; i < numBufs; i++) 
+    for (int i = 0; i < numBufs; i++)
     {
         BufDesc* tmpbuf = &bufTable[i];
         if (tmpbuf->valid == true && tmpbuf->dirty == true) {
@@ -63,7 +64,7 @@ BufMgr::~BufMgr() {
 }
 
 
-const Status BufMgr::allocBuf(int & frame) 
+const Status BufMgr::allocBuf(int & frame)
 {
 
 
@@ -73,21 +74,63 @@ const Status BufMgr::allocBuf(int & frame)
 	return OK;
 }
 
-	
+
 const Status BufMgr::readPage(File* file, const int PageNo, Page*& page)
 {
 	/*
 	 * First impression of how code works, probably wrong
 	 */
+	 //check whether page is already in buffer pool
+    int frameNo = -1;
+    Status s = hashTable->lookup(file, PageNo, frameNo);
+    if(s == HASHNOTFOUND)
+    {
+        //CASE 1: Page not in buffer pool
+        // allocate a buffer frame, read page into buffer frame,
+        // insert page into hashtable, and set it up
+
+        s = allocBuf(frameNo);
+        if(s != OK)
+        {
+            return s;
+        }
+        s = file->readPage(PageNo, page);
+        if(s != OK)
+        {
+            return s;
+        }
+
+        s = hashTable->insert(file, PageNo, frameNo);
+        if(s != OK)
+        {
+            return s;
+        }
+
+        //set up proper frame
+        BufDesc* tmpbuf = &(bufTable[frameNo]);
+        tmpbuf->Set(file, PageNo);
+
+        //return frame pointer
+        page = &(bufPool[frameNo]);
+    }
+    else
+    {
+        //CASE 2: Page is in buffer pool
+        //set refbit, increment pinCnt
+        BufDesc* tmpbuf = &(bufTable[frameNo]);
+        tmpbuf->refbit = true;
+        tmpbuf->pinCnt++;
 
 
-
+        //return frame pointer
+        page = &(bufPool[frameNo]);
+    }
 	return OK;
 }
 
 
-const Status BufMgr::unPinPage(File* file, const int PageNo, 
-			       const bool dirty) 
+const Status BufMgr::unPinPage(File* file, const int PageNo,
+			       const bool dirty)
 {
 	/*
 	 * First impression of how code works, probably wrong
@@ -105,7 +148,7 @@ const Status BufMgr::unPinPage(File* file, const int PageNo,
 	return OK;
 }
 
-const Status BufMgr::allocPage(File* file, int& pageNo, Page*& page) 
+const Status BufMgr::allocPage(File* file, int& pageNo, Page*& page)
 {
 	/*
 	 * First impression of how code works, probably wrong
@@ -118,7 +161,7 @@ const Status BufMgr::allocPage(File* file, int& pageNo, Page*& page)
 	return OK;
 }
 
-const Status BufMgr::disposePage(File* file, const int pageNo) 
+const Status BufMgr::disposePage(File* file, const int pageNo)
 {
     // see if it is in the buffer pool
     Status status = OK;
@@ -135,7 +178,7 @@ const Status BufMgr::disposePage(File* file, const int pageNo)
     return file->disposePage(pageNo);
 }
 
-const Status BufMgr::flushFile(const File* file) 
+const Status BufMgr::flushFile(const File* file)
 {
   Status status;
 
@@ -168,21 +211,21 @@ const Status BufMgr::flushFile(const File* file)
     else if (tmpbuf->valid == false && tmpbuf->file == file)
       return BADBUFFER;
   }
-  
+
   return OK;
 }
 
 
-void BufMgr::printSelf(void) 
+void BufMgr::printSelf(void)
 {
     BufDesc* tmpbuf;
-  
+
     cout << endl << "Print buffer...\n";
     for (int i=0; i<numBufs; i++) {
         tmpbuf = &(bufTable[i]);
-        cout << i << "\t" << (char*)(&bufPool[i]) 
+        cout << i << "\t" << (char*)(&bufPool[i])
              << "\tpinCnt: " << tmpbuf->pinCnt;
-    
+
         if (tmpbuf->valid == true)
             cout << "\tvalid\n";
         cout << endl;
