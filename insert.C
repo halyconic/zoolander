209ab365bf03909cd2231777a9d77fc5d351f36a
Stephen Webber <stephen@riley.(none)>
2012-12-02 14:46:05 -0600

    First code for insert.C

--- insert.C
+++ insert.C
@@ -14,8 +14,39 @@ const Status QU_Insert(const string & relation,
 	const int attrCnt, 
 	const attrInfo attrList[])
 {
-// part 6
-return OK;
+	/*
+	 * Initial code, based on getRelInfo and createRel
+	 *
+	 * Initialize scan
+	 */
+	Status status;
+	RID rid;
+	Record rec;
+	InsertFileScan* ifs;
 
+	// if (relation.empty()) return BADCATPARM;
+
+	if (relation.empty() || attrCnt < 1) return BADCATPARM;
+
+	ifs = new InsertFileScan(ATTRCATNAME, status);
+	if (status != OK) return status;
+
+	/*
+	 * Create record using catalog and offsets
+	 */
+
+	rec = Record();
+	rec.data = NULL;
+	rec.length = 0;
+
+	/*
+	 * Execute scan
+	 */
+
+	status = ifs->insertRecord(rec, rid);
+	if (status != OK) return status;
+
+	// part 6
+	return OK;
 }
 
