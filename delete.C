9b5b5b3f54088a25e79e064b0349fd8598d21af2
Nathan Murray <nkmurray@wisc.edu>
2012-12-02 19:51:19 -0600

    Delete

--- delete.C
+++ delete.C
@@ -10,17 +10,39 @@
  * 	an error code otherwise
  */
 
-const Status QU_Delete(const string & relation, 
-		       const string & attrName, 
+const Status QU_Delete(const string & relation,
+		       const string & attrName,
 		       const Operator op,
-		       const Datatype type, 
+		       const Datatype type,
 		       const char *attrValue)
 {
 // part 6
-return OK;
+    Status status;
+    HeapFileScan*  hfs;
+    RID rid;
+    AttrDesc record;
 
+    attrCat->getInfo(relation, attrName, record);
+    hfs = new HeapFileScan(relation, status);
+    if (status != OK) return status;
 
+    if ((status = hfs->startScan(record.attrOffset, record.attrLen, type,
+              attrValue, op)) != OK)
+    {
+        delete hfs;
+        return status;
+    }
 
+    // find all tuples that match the search
+    while((status = hfs->scanNext(rid)) != FILEEOF)
+    {
+        // remove the tuple
+        status = hfs->deleteRecord();
+        if(status != OK) return status;
+    }
+
+    delete hfs;
+    return OK;
 }
 
 
