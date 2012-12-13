735e958b1056c63e5e0172cc4848ef0610ca6746
Stephen Webber <stephen@riley.(none)>
2012-12-13 01:07:58 -0600

    Some simple queries added

---
+++ db_connect.php
@@ -0,0 +1,13 @@
+<?php
+
+#TODO: Replace DATABASE_URL with url of postgres database or path variable
+
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+
+# Here we establish the connection. Yes, that's all.
+$pg_conn = pg_connect(pg_connection_string_from_database_url()) or die("Error connecting to database!");
+
+?>
---
+++ db_query.php
@@ -0,0 +1,69 @@
+<?php
+
+# This file performs all the queries. Call these functions after connecting to the database and pass in the connection
+
+# This prints into a javascript array. This may not be what you need, but its a good example
+# You can also print out into html for a simpler presentation
+function get_basic_js($pg_conn)
+{
+    $result = pg_query($pg_conn, "SELECT query, stuff FROM table");
+
+    if (!$result) {
+        echo "An error occured.\n";
+        exit;
+    }
+
+    # print items side-by-side into an array, assumes at least one entry exists already
+    print("var stuffArray = [");
+    $first_row = pg_fetch_row($result);
+    print("'$first_row[0] $first_row[1]'");
+    while ($row = pg_fetch_row($result)) {
+        print(",'$row[0] $row[1]'");
+    }
+    print("];");
+}
+
+# Advanced query
+# http://www.postgresql.org/docs/9.1/static/sql-select.html
+function get_advanced($pg_conn)
+{
+    $result = pg_query($pg_conn, "SELECT advanced, attribute FROM member WHERE member.id=1337");
+
+    if (!$result) {
+        echo "An error occured.\n";
+        exit;
+    }
+
+    # print here
+}
+
+# Update query
+# http://www.postgresql.org/docs/9.1/static/sql-insert.html
+function get_update($pg_conn)
+{
+    $result = pg_query($pg_conn, "INSERT INTO yourmom VALUES ('me', 'alex')");
+
+    if (!$result) {
+        echo "An error occured.\n";
+        exit;
+    }
+
+    # print here
+}
+
+# Browse query
+# http://www.postgresql.org/docs/9.1/static/sql-insert.html
+function get_browse($pg_conn)
+{
+    $result = pg_query($pg_conn, "SELECT id FROM bookshelf");
+
+    if (!$result) {
+        echo "An error occured.\n";
+        exit;
+    }
+
+    # print here
+}
+
+?>
+
