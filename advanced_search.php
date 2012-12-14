7ef6de9d405c0d83cd6799f5b71fd97611d2c20d
Stephen Webber <stephen@riley.(none)>
2012-12-14 02:53:12 -0600

    Format table of advanced output

--- advanced_search.php
+++ advanced_search.php
@@ -11,12 +11,27 @@ require_once('db_display.php');
 </head>
 
 <body>
-    <p>
-    <?php
-        # Displays the result of the query
-        get_advanced($pg_conn);
-    ?>
-    </p>
+
+<tr>
+    <td colspan="2" align="center" valign="top">
+        Basic search results (searched by name):<br>
+        <table border="1" width="75%">
+            <tr>
+                <td align="center" bgcolor="#cccccc"><b>Species ID</b></td>
+                <td align="center" bgcolor="#cccccc"><b>Name of Animal</b></td>
+                <td align="center" bgcolor="#cccccc"><b>Number of Animal</b></td>
+                <td align="center" bgcolor="#cccccc"><b>Square Footage Needed</b></td>
+                <td align="center" bgcolor="#cccccc"><b>Liters of water per day</b></td>
+                <td align="center" bgcolor="#cccccc"><b>Exhibit ID</b></td>
+            </tr>
+            <?php
+                # Displays the result of the query
+                get_advanced($pg_conn);
+            ?>
+        </table>
+    </td>
+</tr>
+
 </body>
 
 </html>
