205d00dfac9606a353191dc47fe37a63726fc663
Stephen Webber <stephen@riley.(none)>
2012-12-14 05:53:27 -0600

    bug

--- advanced_search.php
+++ advanced_search.php
@@ -19,12 +19,10 @@ require_once('db_display.php');
         Basic search results (searched by name):<br>
         <table border="1" width="75%">
             <tr>
-                <td align="center" bgcolor="#cccccc"><b>Species ID</b></td>
                 <td align="center" bgcolor="#cccccc"><b>Name of Animal</b></td>
                 <td align="center" bgcolor="#cccccc"><b>Number of Animal</b></td>
                 <td align="center" bgcolor="#cccccc"><b>Square Footage Needed</b></td>
                 <td align="center" bgcolor="#cccccc"><b>Liters of water per day</b></td>
-                <td align="center" bgcolor="#cccccc"><b>Exhibit ID</b></td>
             </tr>
             <?php
                 # Displays the result of the query
