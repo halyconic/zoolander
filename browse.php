ee8e430baf42c9626f3f24e30cd4df0fa2cbbedb
Nathan Murray <nkmurray@wisc.edu>
2012-12-14 02:40:14 -0600

    dang typos, now testing everything but advanced

--- browse.php
+++ browse.php
@@ -9,10 +9,12 @@
      List of all animals in the zoo!:<br>
 	<table border="1" width="75%">
 	  <tr>
+	    <td align="center" bgcolor="#cccccc"><b>Species ID</b></td>
 	    <td align="center" bgcolor="#cccccc"><b>Name of Animal</b></td>
             <td align="center" bgcolor="#cccccc"><b>Number of Animal</b></td>
             <td align="center" bgcolor="#cccccc"><b>Square Footage Needed</b></td>
 	    <td align="center" bgcolor="#cccccc"><b>Liters of water per day</b></td>
+	    <td align="center" bgcolor="#cccccc"><b>Exhibit ID</b></td>
 	  </tr>
 
 <?php
@@ -59,6 +61,6 @@
 </table>
 	</td>
 	</tr>
-	<?php echo "<a href=/"https://cs564.cs.wisc.edu/nmurray/index.html\">Return to index</a>\n"?>
+	<?php echo "<a href=\"https://cs564.cs.wisc.edu/nmurray/index.html\">Return to index</a>\n"?>
 </body>
 </html>
